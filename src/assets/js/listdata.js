10a80135a8c61cfcf12d0d1b6319b6b54b07d643
张鹏 <1582506497@qq.com>
2019-08-01 21:33:22 +0800

    导航切换，数据增加

---
+++ src/assets/js/listdata.js
@@ -0,0 +1,175 @@
+function listImage0() {
+  let listImage0 = [{
+      img: 'http://www.qhtwood.com/uploads/190624/1-1Z624103R2M5.jpg',
+      name: '浅布纹',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190624/1-1Z624103Z45I.jpg',
+      name: '斜布纹（淡黄色）',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190624/1-1Z624103K3a1.jpg',
+      name: '斜布纹（白色）',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190624/1-1Z624103F4A9.jpg',
+      name: '慕斯胡桃',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190624/1-1Z62410361A03.jpg',
+      name: '山纹尤加利',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ6091U0225.jpg',
+      name: '烈日红木',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ6091Kb37.jpg',
+      name: '紫云杉木',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ6091I1132.jpg',
+      name: '栉风沐雨',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ6091F2J2.jpg',
+      name: '雨丝橡木',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ609161V13.jpg',
+      name: '银丝胡桃',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ6091532538.jpg',
+      name: '忆江月负离子',
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180906/1-1PZ6091503344.jpg',
+      name: '香味柏木',
+    },
+  ];
+  return JSON.stringify(listImage0);
+}
+
+function listImage1() {
+  let listImage1 = [{
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH511053U41.jpg',
+      name: '拉手'
+    }, {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110520W3.jpg',
+      name: '拉手'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH511045aL.jpg',
+      name: '拉手'
+    }, {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110441558.jpg',
+      name: '拉手'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110422256.jpg',
+      name: '拉手'
+    }, {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH511035N45.jpg',
+      name: '拉手'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH511033a31.jpg',
+      name: '拉手'
+    }, {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110321P0.jpg',
+      name: '拉手'
+    },
+    {
+
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110301203.jpg',
+      name: '拉手'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110244G4.jpg',
+      name: '拉手'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110224508.jpg',
+      name: '拉手'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/190725/1-1ZH5110206406.jpg',
+      name: '拉手'
+    },
+  ];
+  return JSON.stringify(listImage1);
+}
+
+function listImage2() {
+  let listImage2 = [{
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100U9363C.jpg',
+      name: '勾线'
+    }, {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100UA1U2.jpg',
+      name: '大顶角线'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100U9363C.jpg',
+      name: 'L型门边线'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/181028/1-1Q02Q53223b0.jpg',
+      name: '装饰线条'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100Z34T07.jpg',
+      name: '小内扣线'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100Z321323.jpg',
+      name: '上下顶线'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100Z21W35.jpg',
+      name: '内嵌线'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100Z1495Y.jpg',
+      name: '门套线'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100Z10Ea.jpg',
+      name: '罗马柱头'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100Z03N18.jpg',
+      name: '罗马柱'
+    },
+    {
+      img: 'http://www.qhtwood.com/uploads/180910/1-1P9100U9363C.jpg',
+      name: '卡条'
+    }
+  ];
+  return JSON.stringify(listImage2);
+}
+
+function listImage3() {
+  let listImage3 = [{
+    img: 'http://www.qhtwood.com/uploads/180910/1-1P9100U434303.jpg',
+    name: '白乳胶'
+  }, {
+    img: 'http://www.qhtwood.com/uploads/180910/1-1P9100U356302.jpg',
+    name: '免钉胶'
+  }];
+  return JSON.stringify(listImage3);
+}
+function listImage4() {
+  let listImage4 = [{
+    img: 'http://www.qhtwood.com/uploads/190305/1-1Z30519221I39.jpg',
+    name: '石膏板'
+  }];
+  return JSON.stringify(listImage4);
+}
+export {
+  listImage0,
+  listImage1,
+  listImage2,
+  listImage3,
+  listImage4
+}
--- src/views/chanpinzhongxin/chanpinzhongxin.vue
+++ src/views/chanpinzhongxin/chanpinzhongxin.vue
@@ -3,56 +3,46 @@
     <div id="info" class="info_bg">
       <div class="container">
         <div class="i-product">
-          <h3 class="info-title"><span>产品中心</span><small>美是我们的外表，健康是我们的内在，环保是我们的终极追求</small></h3>
+          <h3 class="info-title"><span>产品中心</span><small style="line-height:22px;">美是我们的外表，健康是我们的内在，环保是我们的终极追求</small></h3>
+          <!-- 导航部分s -->
+          <nav class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-collapse">
+                  <span class="sr-only">切换导航</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">产品分类</a>
+              </div>
+              <div class="collapse navbar-collapse" id="example-navbar-collapse">
+                <ul class="nav navbar-nav">
+                  <li><a @click.prevent></a></li>
+                  <li><a @click.prevent></a></li>
+                  <li :class="isActive==0?'chanping_active':''"><a href="#" @click.prevent="handleCai(0)">生态板</a></li>
+                  <li><a @click.stop></a></li>
+                  <li :class="isActive==1?'chanping_active':''"><a href="#" @click.prevent="handleCai(1)">五金辅材</a></li>
+                  <li><a @click.stop></a></li>
+                  <li :class="isActive==2?'chanping_active':''"><a href="#" @click.prevent="handleCai(2)">线条</a></li>
+                  <li><a @click.stop></a></li>
+                  <li :class="isActive==3?'chanping_active':''"><a href="#" @click.prevent="handleCai(3)">胶水</a></li>
+                  <li><a @click.stop></a></li>
+                  <li :class="isActive==4?'chanping_active':''"><a href="#" @click.prevent="handleCai(4)">石膏板</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <!-- 导航部分e -->
           <!-- 线条s -->
           <div class="product">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  wow fadeInRightSmall" style="padding-bottom: 5rem;">
               <ul class="pro-list">
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100U9363C.jpg" alt="勾线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">勾线</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100UA1U2.jpg" alt="大顶角线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">大顶角线</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100U6013X.jpg" alt="型门边线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">L型门边线</h5>
+                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6" v-for="(item,index) in listImages" :key="index">
+                  <p><img :src="item.img" :alt="item.name" class="img-responsive" /></p>
+                  <h5 class="chanping_font">{{item.name}}</h5>
                 </li>
 
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/181028/1-1Q02Q53223b0.jpg" alt="装饰线条" class="img-responsive" /></p>
-                  <h5 class="chanping_font">装饰线条</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100Z34T07.jpg" alt="小内扣线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">小内扣线</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100Z321323.jpg" alt="上下顶线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">上下顶线</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100Z21W35.jpg" alt="内嵌线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">内嵌线</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100Z1495Y.jpg" alt="门套线" class="img-responsive" /></p>
-                  <h5 class="chanping_font">门套线</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100Z10Ea.jpg" alt="罗马柱头" class="img-responsive" /></p>
-                  <h5 class="chanping_font">罗马柱头</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100Z03N18.jpg" alt="罗马柱" class="img-responsive" /></p>
-                  <h5 class="chanping_font">罗马柱</h5>
-                </li>
-                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                  <p><img src="http://www.qhtwood.com/uploads/180910/1-1P9100U9363C.jpg" alt="卡条" class="img-responsive" /></p>
-                  <h5 class="chanping_font">卡条</h5>
-                </li>
               </ul>
             </div>
           </div>
@@ -97,6 +87,13 @@
 </template>
 
 <script>
+  import {
+    listImage0,
+    listImage1,
+    listImage2,
+    listImage3,
+    listImage4
+  } from '../../assets/js/listdata.js'
   export default {
     name: '',
 
@@ -110,15 +107,17 @@
 
     data() {
       return {
-
+        isActive: 0,
+        listImages: [],
       }
     },
 
     computed: {},
 
     watch: {},
-
-    created() {},
+    created() {
+      this.listImages = JSON.parse(listImage0());
+    },
 
     mounted() {
 
@@ -131,6 +130,28 @@
         console.log(e)
 
       },
+      handleCai: function(n) {
+        let listData = '';
+        switch (parseInt(n)) {
+          case 0:
+            listData = JSON.parse(listImage0());
+            break;
+          case 1:
+            listData = JSON.parse(listImage1());
+            break;
+          case 2:
+            listData = JSON.parse(listImage2());
+            break;
+          case 3:
+            listData = JSON.parse(listImage3());
+            break;
+          case 4:
+            listData = JSON.parse(listImage4());
+
+        }
+        this.listImages = listData;
+        this.isActive = n;
+      }
     }
   };
 </script>
@@ -141,6 +162,14 @@
   }
 
   .chanping {
+    &_active {
+      background: #44914a;
+    }
+
+    &_active a {
+      color: #fff !important;
+    }
+
     &_font {
       padding: 1.4rem;
       background: #f7f7f7;
