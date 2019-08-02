75dc501fd2887c300ae1d5843863a5034949700b
张鹏 <1582506497@qq.com>
2019-08-02 23:03:21 +0800

    yangshi

--- src/views/huanbaobancai/huanbaobancai.vue
+++ src/views/huanbaobancai/huanbaobancai.vue
@@ -1,43 +1,33 @@
 <template>
   <div class="haunbaobancai">
-     <div class="img_responsive_box" @click="handleImg" v-if="isShow">
-      <img :src="bigImg" class="img-responsive" alt="响应式图像" @click.stop>
-    </div>
-    <!-- info -->
-
-   <div id="info" class="info_bg">
+    <!-- 主体部分s -->
+    <div id="info">
       <div class="container">
-        <div class="i-case">
-          <h3 class="info-title"><span>环保板材</span><small></small></h3>
-        </div>
-        <ul class="case-list gallery"></ul></div></div>
-            <div class="pro-title1" align="center">
-              <div class="pro-title">
-                <div class="t1">楠香木</div>
-                <div class="t2">A+级饰面选样标准 至臻自然亦科技</div>
-                <div class="t3">查看详情<span class="pline"></span></div>
-              </div>
-              <a href="#" title="楠香木生态免漆板">
-                <img src="/static/utils/uploads/huanbaobancai//1.jpg" alt="楠香木生态免漆板" width="1200" height="600" align="center">
-              </a>
+
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 pre" style="padding: 0;">
+            <img src="/static/utils/uploads/huanbaobancai//1.jpg" class="bao_big_img" alt="楠香木生态免漆板" style="width: 100%;">
+            <div class="pao bao_big_box">
+              <div class="bao_big_box_cai">环保板材</div>
+              <div class="bao_big_box_ke">A+级饰面选样标准 至臻自然亦科技</div>
+              <div>查看详情</div>
             </div>
-    <br/>
-          <div class="product-list">
-            <li class="col-lg-4 col-md-4 col-sm-4 col-xs-6" v-for="(item,index) in images" :key="index"  @click.stop="handleClose(item.img)">
-               <div class="item" data-type="item.t1">
-                <div class="pro-title">
-                  <div class="t1">{{item.t1}}</div>
-                  <div class="t2">{{item.t2}}</div>
-                  <div class="t3">{{item.t3}}<span class="pline"></span></div>
-                </div>
-              </div>
-              <div class="img"><img :src="item.img" :alt="item.img"  width="580" height="420"/></div>
-            </li>
           </div>
-        <div class="page">
-          <span class="pageinfo">共 <strong>1</strong>页<strong>{{images.length}}</strong>条记录</span>
-        </div>
+          <div class="bao_big_bg pre">
+            <div v-for="(item,index) in images" :key="index" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pre bao_big_bg_color"
+              style="padding: 0;">
+              <div class="bao_big_bg_font">
+                <div class="bao_big_bg_font1">{{item.t1}}</div>
+                <div class="bao_big_bg_font2">{{item.t2}}</div>
+                <div class="bao_big_bg_font3">{{item.t3}}</div>
+              </div>
+              <img :src="item.img" alt="" class="bao_big_bg_img">
+            </div>
+          </div>
+
       </div>
+    </div>
+    <!-- 主体部分e -->
+  </div>
 </template>
 
 <script>
@@ -57,49 +47,47 @@
           t1: '生态免漆板',
           t2: '一面科技 一面艺术',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/3.png',
           t1: '3D实木板',
           t2: '3D实木板',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/4.png',
           t1: '饰面板',
           t2: '饰面板',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/5.png',
           t1: '细木工板',
           t2: '细木工板',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/6.png',
           t1: '多层板',
           t2: '多层板',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/7.png',
           t1: 'OSB板',
           t2: 'OSB板',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/8.png',
           t1: '阻燃板',
           t2: '阻燃板',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/9.png',
           t1: '集成材',
           t2: '集成材',
           t3: '查看详情'
-        },{
+        }, {
           img: '/static/utils/uploads/huanbaobancai/10.png',
           t1: '石膏板',
           t2: '石膏板',
           t3: '查看详情'
         }],
-        isShow:false,
-        bigImg:''
       }
     },
 
@@ -120,23 +108,69 @@
 </script>
 
 <style lang="less">
-  .img_responsive_box {
-    position: fixed;
-    top: 0px;
-    left: 0rpx;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-    background: rgba(0, 0, 0, .8);
-    z-index: 10000;
+  .pre {
+    position: relative
+  }
 
-    .img-responsive {
-      display: inline-block;
-      height: auto;
-      max-width: 100%;
+  .pao_lt0 {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .dis_y_center {
+    display: flex;
+    flex-direction: column;
+    align-items: center
+  }
+
+  .pao {
+    .dis_y_center();
+    .pao_lt0();
+    width: 100%;
+    height: 100%;
+    padding-top: 5%;
+    box-sizing: border-box;
+  }
+
+  .bao_big_box {
+    color: #fff;
+
+    &_cai {
+      font-size: 24px;
+      line-height: 40px;
+      margin-bottom: 5px;
+      letter-spacing: 8px;
+      font-weight: 600;
+    }
+
+    ;
+
+    &_img {
+      width: 100%;
+    }
+  }
+
+  .bao_big_bg {
+    width: 100%;
+    box-sizing: border-box;
+
+    &_color {
+      background: #F5F5F5;
+    }
+
+    &_img {
+      width: 100%;
+      background: #fff;
+    }
+
+    &_font {
+      width: 100%;
+      height: 30%;
+      box-sizing: border-box;
+      .dis_y_center();
+      padding-top: 15%;
+      background: #fff;
     }
   }
 </style>
